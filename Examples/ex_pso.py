35be766b377999c41da9379b74e4daa69d16a030
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2019-05-16 16:27:23 -0300

    Include concept of acceleration to process
    Now particles have some inertia to mantain its own trajectory.
    This is important when all random initial solutions are located in a cluster far from the answer.
    In this case, particles won't collide to an mutual answer in between them, but instead they will orbit the mean point and look for options around it.

--- Examples/ex_pso.py
+++ Examples/ex_pso.py
@@ -12,13 +12,13 @@ xmin = 0.
 ymax = 10.
 ymin = 0.
 
-# Velocities
+# Speed constants
 vp = .3
 vg = .2
 
 # Control variables
-tol = 3.
-max_gen = 10
+tol = .3
+max_gen = 50
 gen = 0
 
 list_ind = []
@@ -26,6 +26,8 @@ list_ind = []
 real = (3., 5.)
 
 p_best = []
+
+v = []
 
 plt.plot(real[0], real[1], 'gx')
 plt.axis([xmin, xmax, ymin, ymax])
@@ -42,23 +44,35 @@ for i in range(pop_size):
                      abs(real[1] - list_ind[i][1][1]*(ymax - ymin) + ymin)
 
     # Create personal best vector
-    p_best.append(list_ind[i])
+    p_best.append(copy.copy(list_ind[i]))
+
+    # Initialize speed of particles
+    v.append((0., 0.))
 
     # Plot new population
     plt.plot(list_ind[i][1][0] * (xmax - xmin) + xmin, list_ind[i][1][1] * (ymax - ymin) + ymin,
-             color[gen % len(color)] + marker[gen % len(marker)])
+             color[i] + '*')
 
 # Create global best
 g_best = copy.copy(sorted(list_ind)[0])
 
-while gen < max_gen:
+while gen < max_gen and g_best[0] > tol:
 
     gen += 1
 
     # Update coordinates of individuals
     for i in range(pop_size):
-        list_ind[i][1] = (list_ind[i][1][0] + vp*(p_best[i][1][0] - list_ind[i][1][0]) + vg*(g_best[1][0] - list_ind[i][1][0]),
-                          list_ind[i][1][1] + vp*(p_best[i][1][1] - list_ind[i][1][1]) + vg*(g_best[1][1] - list_ind[i][1][1]))
+
+        # Calculate acceleration of particles
+        a = (vp*(p_best[i][1][0] - list_ind[i][1][0]) + vg*(g_best[1][0] - list_ind[i][1][0]),
+             vp*(p_best[i][1][1] - list_ind[i][1][1]) + vg*(g_best[1][1] - list_ind[i][1][1]))
+
+        # Update position of particles
+        list_ind[i][1] = (min(max(list_ind[i][1][0] + v[i][0] + a[0], xmin), xmax),
+                          min(max(list_ind[i][1][1] + v[i][1] + a[1], ymin), ymax))
+
+        # Update speed of particles
+        v[i] = (v[i][0] + a[0], v[i][1] + a[1])
 
         # Update fitness value
         list_ind[i][0] = abs(real[0] - list_ind[i][1][0] * (xmax - xmin) + xmin) + \
@@ -66,11 +80,11 @@ while gen < max_gen:
 
         # Update personal best vector
         if p_best[i][0] > list_ind[i][0]:
-            p_best[i] = list_ind[i]
+            p_best[i] = copy.copy(list_ind[i])
 
         # Plot new population
         plt.plot(list_ind[i][1][0]*(xmax - xmin) + xmin, list_ind[i][1][1]*(ymax - ymin) + ymin,
-                 color[gen % len(color)] + marker[gen % len(marker)])
+                 color[i] + marker[i])
 
     if g_best[0] > min(list_ind)[0]:
         g_best = copy.copy(sorted(list_ind)[0])
@@ -78,6 +92,8 @@ while gen < max_gen:
 for p in p_best:
     print p
 
-print g_best
+print gen
+
+print "**", g_best, "**"
 
 plt.show()
