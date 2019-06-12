c6e89e8eae701ed0cdd7f64fdc987441d62e9940
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2019-06-12 15:54:24 -0300

    Add speeds to dic

--- PSO.py
+++ PSO.py
@@ -26,7 +26,7 @@ def Function(dic, tolerance):
     ERROR = __import__(dic['chsn_err'])
 
     # Population size
-    pop_size = dic['PSO']['population']
+    swarm_size = dic['PSO']['swarm_size']
 
     # Boundaries
     up_bound = dic['PSO']['p_max']
@@ -39,19 +39,21 @@ def Function(dic, tolerance):
     ymin = 0.
 
     # Speed constants
-    vp = .3
-    vg = .2
+    vp = dic['PSO']['p_speed']
+    vg = dic['PSO']['g_speed']
 
     # Control variables
     max_it = dic['PSO']['max_iteration']
     iter = 0
 
-    list_ind = []
+    particles = []
 
     if not dic['import_data']:
         op_real = SIM.rk4(dic, dic['real'])
     else:
         op_real = dic['u'][:, [0, 3, 4]]
+
+    real = (3., 6.)
 
     p_best = []
 
@@ -61,38 +63,38 @@ def Function(dic, tolerance):
     style.use('ggplot')
 
     # Create figure
-    # fig = plt.figure()
-    # ax1 = fig.add_subplot(1, 1, 1)
-    # ax1.axis([xmin, xmax, ymin, ymax])
+    fig = plt.figure()
+    ax1 = fig.add_subplot(1, 1, 1)
+    ax1.axis([xmin, xmax, ymin, ymax])
 
     # Plotting real values
-    # ax1.plot(real[0], real[1], 'gx')
+    ax1.plot(real[0], real[1], 'gx')
 
     color = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
     marker = ['.', 'v', '^', '<', '>', '1', '2', '3', '4', 's', 'p', '*', 'h', 'H', '+', 'D', 'd']
 
-    # Create random population
-    for i in range(pop_size):
-        list_ind.append([0, (np.random.random(), np.random.random())])
+    # Create random swarm
+    for i in range(swarm_size):
+        particles.append([0, (np.random.random(), np.random.random())])
 
-        # Evaluate fitness of individual
-        list_ind[i][0] = .5*dic['TS']['step'] * \
-                         ERROR.Error(op_real, SIM.rk4(dic, (list_ind[i][1]*(up_bound - low_bound) + low_bound)))
-
-        print list_ind
+        # Evaluate fitness of particle
+        particles[i][0] = .5*dic['TS']['step'] * \
+                         ERROR.Error(op_real, SIM.rk4(dic, (particles[i][1]*(up_bound - low_bound) + low_bound)))
 
         # Create personal best vector
-        p_best.append(copy.copy(list_ind[i]))
+        p_best.append(copy.copy(particles[i]))
 
         # Initialize speed of particles
         v.append((0., 0.))
 
         # Plot new population
-        # ax1.plot(list_ind[i][1][0] * (xmax - xmin) + xmin, list_ind[i][1][1] * (ymax - ymin) + ymin,
-        #          color[i % len(color)] + '*')
+        ax1.plot((particles[i][1]*(up_bound - low_bound) + low_bound)[0], (particles[i][1]*(up_bound - low_bound) + low_bound)[1],
+                 color[i % len(color)] + '*')
 
     # Create global best
-    g_best = copy.copy(sorted(list_ind)[0])
+    g_best = copy.copy(sorted(particles)[0])
+
+    plt.show()
 
     while iter < max_it and g_best[0] > tolerance:
 
@@ -105,36 +107,36 @@ def Function(dic, tolerance):
         iter += 1
 
         # Update coordinates of individuals
-        for i in range(pop_size):
+        for i in range(swarm_size):
 
             # Calculate acceleration of particles
-            a = (vp*(p_best[i][1][0] - list_ind[i][1][0]) + vg*(g_best[1][0] - list_ind[i][1][0]),
-                 vp*(p_best[i][1][1] - list_ind[i][1][1]) + vg*(g_best[1][1] - list_ind[i][1][1]))
+            a = (vp*(p_best[i][1][0] - particles[i][1][0]) + vg*(g_best[1][0] - particles[i][1][0]),
+                 vp*(p_best[i][1][1] - particles[i][1][1]) + vg*(g_best[1][1] - particles[i][1][1]))
 
             # Update position of particles
-            list_ind[i][1] = (min(max(list_ind[i][1][0] + v[i][0] + a[0], xmin), xmax),
-                              min(max(list_ind[i][1][1] + v[i][1] + a[1], ymin), ymax))
+            particles[i][1] = (min(max(particles[i][1][0] + v[i][0] + a[0], xmin), xmax),
+                              min(max(particles[i][1][1] + v[i][1] + a[1], ymin), ymax))
 
             # Update speed of particles
             v[i] = (v[i][0] + a[0], v[i][1] + a[1])
 
             # Update fitness value
-            list_ind[i][0] = abs(real[0] - list_ind[i][1][0] * (xmax - xmin) + xmin) + \
-                             abs(real[1] - list_ind[i][1][1] * (ymax - ymin) + ymin)
+            particles[i][0] = abs(real[0] - particles[i][1][0] * (xmax - xmin) + xmin) + \
+                             abs(real[1] - particles[i][1][1] * (ymax - ymin) + ymin)
 
             # Update personal best vector
-            if p_best[i][0] > list_ind[i][0]:
-                p_best[i] = copy.copy(list_ind[i])
+            if p_best[i][0] > particles[i][0]:
+                p_best[i] = copy.copy(particles[i])
 
             # Plot new population
-            ax1.plot(list_ind[i][1][0]*(xmax - xmin) + xmin, list_ind[i][1][1]*(ymax - ymin) + ymin,
+            ax1.plot(particles[i][1][0]*(xmax - xmin) + xmin, particles[i][1][1]*(ymax - ymin) + ymin,
                      color[i % len(color)] + marker[i % len(marker)])
 
         # Plotting real values
         ax1.plot(real[0], real[1], 'gx')
 
-        if g_best[0] > min(list_ind)[0]:
-            g_best = copy.copy(sorted(list_ind)[0])
+        if g_best[0] > min(particles)[0]:
+            g_best = copy.copy(sorted(particles)[0])
 
     print iter
 
