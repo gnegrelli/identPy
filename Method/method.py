919982ada8c6555bafcf2d14f044307951f9838c
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2019-12-17 20:52:20 -0300

    Add imports and init error_log

--- Method/method.py
+++ Method/method.py
@@ -1,10 +1,14 @@
 from abc import ABC
 
+import numpy as np
+import time
+
 
 class Method(ABC):
 
     def __init__(self):
         self.counter = 0
+        self.error_log = []
 
     def __call__(self, *args, **kwargs):
         pass
