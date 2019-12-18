9cc6bbfd7859f96e393810a7489dc8f46092acac
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2019-12-18 17:58:29 -0300

    Initialize MVMO tolerance

--- Method/MVMO.py
+++ Method/MVMO.py
@@ -8,7 +8,7 @@ import random
 
 class MVMO(Method):
     def __init__(self, lo_p, hi_p, pop_sz=5, offsp_sz=1, max_gen=5000, d=5, delta_d=1.5, fs=1, rnd=False, seq_rnd=False,
-                 mv_wndw=True, block=True, wndw_sz=1, wndw_step=1):
+                 mv_wndw=True, block=True, wndw_sz=1, wndw_step=1, tol=1.5):
 
         assert isinstance(lo_p, np.ndarray), "Lower boundary of parameters must be a numpy array"
         assert isinstance(hi_p, np.ndarray), "Upper boundary of parameters must be a numpy array"
@@ -31,6 +31,8 @@ class MVMO(Method):
 
         self.wndw_sz = wndw_sz
         self.wndw_step = wndw_step
+
+        self.tol = tol
 
         super().__init__()
 
