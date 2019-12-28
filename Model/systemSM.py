04292ceae291ce01aa92573638ec7b3a660be039
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2019-12-28 13:37:49 -0300

    Remove deprecated code

--- Model/systemSM.py
+++ Model/systemSM.py
@@ -57,27 +57,3 @@ class SpringMass(Model):
         g2 = x[1]
 
         return np.array([[g1], [g2]])
-
-
-
-def Matrix(p, x0, u0):
-    
-    import numpy as np
-
-    """
-    p[0] = k
-    p[1] = m
-
-    u = [F]
-
-    x = [x, v]
-    """
-    A = np.array([[0., 1.],[-p[1]/p[0], 0.]])
-    
-    B = np.array([[0.],[1./p[0]]])
-    
-    C = np.array([[1, 0],[0, 1]])
-
-    D = 0
-    
-    return A, B, C, D
