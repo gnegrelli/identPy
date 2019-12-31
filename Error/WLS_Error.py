3654884ebbe9bae963ad9fe0e1c10951bd3d174c
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2019-12-31 15:49:56 -0300

    Add weights to least square method

--- Error/WLS_Error.py
+++ Error/WLS_Error.py
@@ -1,9 +1,12 @@
-def ls_error(y, x):
+def wls_error(y, x, w=None):
     
+    if w is None:
+        w = np.ones((y.shape[0], 1))
+
     err = 0
-    
+
     if y.shape == x.shape:
-        for i in (y - x)**2:
+        for i in w*(y - x)**2:
             for j in i:
                 err += j
         return err
@@ -15,10 +18,10 @@ import numpy as np
 
 
 if __name__ == '__main__':
-    w = np.array([[1], [2]])
-    a = np.array([[1, 2], [3, 4]])
-    b = np.array([[5, 6], [7, 8]])
+    w = np.array([[1], [2], [1]])
+    a = np.array([[1, 2], [3, 4], [10, 11]])
+    b = np.array([[5, 6], [7, 8], [13, 49]])
     print(a.shape, b.shape)
     for i in w*((a - b) ** 2):
         print(i)
-    print(ls_error(a, b))
+    print(wls_error(a, b, w))
