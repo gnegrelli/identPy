8186ba9c5c443452bedd3f29fddc0c54ec24c9aa
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2019-12-31 15:42:38 -0300

    Add small test script

--- Error/LS_Error.py
+++ Error/LS_Error.py
@@ -9,3 +9,16 @@ def ls_error(y, x):
         return err
     else:
         print("Error: Arrays must have the same shapes")
+
+
+import numpy as np
+
+
+if __name__ == '__main__':
+    w = np.array([[1], [2]])
+    a = np.array([[1, 2], [3, 4]])
+    b = np.array([[5, 6], [7, 8]])
+    print(a.shape, b.shape)
+    for i in w*((a - b) ** 2):
+        print(i)
+    print(ls_error(a, b))
