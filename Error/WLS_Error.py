c884a3b67e792a80ecbf3864c461db64b6f803e8
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2019-12-31 15:55:47 -0300

    Assert number of rows of weight array

--- Error/WLS_Error.py
+++ Error/WLS_Error.py
@@ -5,6 +5,8 @@ def wls_error(y, x, w=None):
     if w is None:
         w = np.ones((y.shape[0], 1))
 
+    assert w.shape[0] == y.shape[0], "Weight array must have the same number of rows as y and x"
+
     err = 0
 
     for i in w*(y - x)**2:
@@ -20,7 +22,4 @@ if __name__ == '__main__':
     w = np.array([[1], [2], [1]])
     a = np.array([[1, 2], [3, 4], [10, 11]])
     b = np.array([[5, 6], [7, 8], [13, 49]])
-    print(a.shape, b.shape)
-    for i in w*((a - b) ** 2):
-        print(i)
     print(wls_error(a, b, w))
