76f54270e3153b98d459e65f7db53b5329bcdb57
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2020-01-03 19:01:50 -0300

    Add TS as estimation method

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from Model.Implicit_Methods.RK4 import RK4
 
 from Method.MVMO import MVMO
 from Method.PSO import PSO
+from Method.TS import TS
 
 from estimator import Estimator
 
@@ -40,6 +41,7 @@ a.update_output(p=np.array([3.5, 6]))
 
 m = MVMO(np.array([0, 0]), np.array([10, 10]))
 m = PSO(np.array([0, 0]), np.array([10, 10]))
+m = TS(np.array([4, 5]))
 
 est = Estimator(y_meas, a, m)
 
