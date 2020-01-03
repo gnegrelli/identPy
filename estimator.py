b9bd407ea9c11fbedf017453041220de8201ea55
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2020-01-03 13:31:05 -0300

    Create Estimator property real output

--- estimator.py
+++ estimator.py
@@ -5,9 +5,12 @@ from Method.method import Method
 
 class Estimator:
 
-    def __init__(self, model, method):
+    def __init__(self, real_output, model, method):
         assert isinstance(model, Model), "Model must be of class Model"
         assert isinstance(method, Method), "Method must be of class Method"
+        assert isinstance(real_output, np.ndarray), "Real output must be a numpy array"
+
+        self.y_meas = real_output
 
         self.model = model
         self.method = method
