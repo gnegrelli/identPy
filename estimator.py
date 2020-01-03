950e86adcc9da7760907428ccf4e29e3851f85c1
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2020-01-03 11:31:40 -0300

    Assert types of model and method

--- estimator.py
+++ estimator.py
@@ -1,9 +1,14 @@
 import numpy as np
+from Model.model import Model
+from Method.method import Method
 
 
 class Estimator:
 
     def __init__(self, model, method):
+        assert isinstance(model, Model), "Model must be of class Model"
+        assert isinstance(method, Method), "Method must be of class Method"
+
         self.model = model
         self.method = method
 
