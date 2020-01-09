b7bf389d2e998b937dacfe61c11f5fb5db23bd24
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2020-01-09 18:38:17 -0300

    Add Linearized Z-IM class

--- Model/systemZIM.py
+++ Model/systemZIM.py
@@ -1,39 +1,80 @@
- # -*- coding: utf-8 -*-
-"""
-Created on Thu Apr 25 23:36:44 2017
-Z-IM Load Model System Matrices
-University of Sao Paulo
-@author: Gabriel
-"""
+from Model.model import Model
+
+import numpy as np
 
 
-def Matrix(p, x0, u0):
-    
-    import numpy as np
-    
-    """
-    p[0] = X
-    p[1] = X'
-    p[2] = T0
-    p[3] = M
-    p[4] = Gs
-    p[5] = Bs
-    p[6] = E0
-    p[7] = delta0
-    """
+# TODO: Test class and confirm equations
+class ZIM(Model):
 
-    A = np.array([[-p[0]/(p[1]*p[2]), -(p[0]-p[1])*u0[0]*np.sin(p[7])/(p[1]*p[2]), 0],
-                  [(p[0]-p[1])*u0[0]*np.sin(p[7])/(p[1]*p[2]*(p[6]**2)), -(p[0]-p[1])*u0[0]*np.cos(p[7])/(p[1]*p[2]*p[6]), 1],
-                  [-u0[0]*np.sin(p[7])/(p[3]*p[1]), -u0[0]*p[6]*np.cos(p[7])/(p[3]*p[1]), 0]])
-    
-    B = np.array([[(p[0]-p[1])*np.cos(p[7])/(p[1]*p[2])],
-                  [-(p[0]-p[1])*np.sin(p[7])/(p[1]*p[2]*p[6])],
-                  [-p[6]*np.sin(p[7])/(p[1]*p[3])]])
-    
-    C = np.array([[-u0[0]*np.sin(p[7])/p[1], -u0[0]*p[6]*np.cos(p[7])/p[1], 0],
-                  [-u0[0]*np.cos(p[7])/p[1], u0[0]*p[6]*np.sin(p[7])/p[1], 0]])
-    
-    D = np.array([[2*p[4]*u0[0]-(p[6]*np.sin(p[7])/p[1])],
-                  [2*p[5]*u0[0]+(2*u0[0]-p[6]*np.cos(p[7]))/p[1]]])
-    
-    return A, B, C, D
+    def __init__(self, x_0=0, u=0, method=None):
+
+        super().__init__(x_0, u, method)
+
+        self.parameters = {
+            'X': 'Reactance',
+            "X'": 'Reactance',
+            'T_0': 'Time constant',
+            'M': 'Inertia',
+            'G_s': 'Admittance',
+            'B_s': 'Susceptance',
+            'E_0': 'Initial voltage',
+            'δ_0': 'Initial angle',
+        }
+
+        self.inputs = {
+            'ΔV': 'Voltage variation',
+        }
+
+        self.outputs = {
+            'ΔP': 'Active power variation',
+            'ΔQ': 'Reactive power variation',
+        }
+
+    def f(self, x=None, u=None):
+
+        # TODO: Move asserts and x and u designation to super
+
+        if x is None:
+            x = self.x_0
+        if u is None:
+            u = self.u
+
+        assert isinstance(x, np.ndarray), "States vector must be given in a numpy array"
+        assert isinstance(u, np.ndarray), "Input vector must be given in a numpy array"
+
+        f1 = - self.p[0]/(self.p[1] * self.p[2])*x[0] \
+             - (self.p[0] - self.p[1])*self.u[0]*np.sin(self.p[7])/(self.p[1]*self.p[2])*x[1] \
+             + (self.p[0] - self.p[1])*np.cos(self.p[7])/(self.p[1]*self.p[2])*u[0]
+
+        f2 = (self.p[0] - self.p[1])*self.u[0]*np.sin(self.p[7])/(self.p[1] * self.p[2]*(self.p[6] ** 2))*x[0] \
+             - (self.p[0] - self.p[1])*self.u[0]*np.cos(self.p[7])/(self.p[1]*self.p[2]*self.p[6])*x[1] \
+             + x[2] \
+             - (self.p[0] - self.p[1])*np.sin(self.p[7])/(self.p[1]*self.p[2]*self.p[6])*u[0]
+
+        f3 = - self.u[0]*np.sin(self.p[7])/(self.p[3]*self.p[1])*x[0] \
+             - self.u[0]*self.p[6]*np.cos(self.p[7])/(self.p[3]*self.p[1])*x[1] \
+             - self.p[6]*np.sin(self.p[7])/(self.p[1]*self.p[3])*u[0]
+
+        return np.array([f1, f2, f3])
+
+    def g(self, x=None, u=None):
+
+        # TODO: Move asserts and x and u designation to super
+
+        if x is None:
+            x = self.x_0
+        if u is None:
+            u = self.u
+
+        assert isinstance(x, np.ndarray), "States vector must be given in a numpy array"
+        assert isinstance(u, np.ndarray), "Input vector must be given in a numpy array"
+
+        g1 = - self.u[0]*np.sin(self.p[7])/self.p[1]*x[0] \
+             - self.u[0]*self.p[6]*np.cos(self.p[7])/self.p[1]*x[1] \
+             + 2*self.p[4]*self.u[0] - (self.p[6]*np.sin(self.p[7])/self.p[1])*u[0]
+
+        g2 = - self.u[0]*np.cos(self.p[7])/self.p[1]*x[0] \
+             + self.u[0]*self.p[6]*np.sin(self.p[7])/self.p[1]*x[1] \
+             + 2*self.p[5]*self.u[0] + (2*self.u[0] - self.p[6]*np.cos(self.p[7]))/self.p[1]*u[0]
+
+        return np.array([g1, g2])
