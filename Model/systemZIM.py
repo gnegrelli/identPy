48c80bb1f7e598e5235f88047fd370c1f6bcde80
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2020-01-21 17:48:07 -0300

    Adjust ZIM equations to use external input data

--- Model/systemZIM.py
+++ Model/systemZIM.py
@@ -35,17 +35,17 @@ class ZIM(Model):
         x, u = super().f(x, u)
 
         f1 = - self.p[0]/(self.p[1] * self.p[2])*x[0] \
-             - (self.p[0] - self.p[1])*self.u[0]*np.sin(self.p[7])/(self.p[1]*self.p[2])*x[1] \
-             + (self.p[0] - self.p[1])*np.cos(self.p[7])/(self.p[1]*self.p[2])*u[0]
+             - (self.p[0] - self.p[1])*1*np.sin(self.p[7])/(self.p[1]*self.p[2])*x[1] \
+             + (self.p[0] - self.p[1])*np.cos(self.p[7])/(self.p[1]*self.p[2])*u[1]
 
-        f2 = (self.p[0] - self.p[1])*self.u[0]*np.sin(self.p[7])/(self.p[1] * self.p[2]*(self.p[6] ** 2))*x[0] \
-             - (self.p[0] - self.p[1])*self.u[0]*np.cos(self.p[7])/(self.p[1]*self.p[2]*self.p[6])*x[1] \
+        f2 = (self.p[0] - self.p[1])*1*np.sin(self.p[7])/(self.p[1] * self.p[2]*(self.p[6] ** 2))*x[0] \
+             - (self.p[0] - self.p[1])*1*np.cos(self.p[7])/(self.p[1]*self.p[2]*self.p[6])*x[1] \
              + x[2] \
-             - (self.p[0] - self.p[1])*np.sin(self.p[7])/(self.p[1]*self.p[2]*self.p[6])*u[0]
+             - (self.p[0] - self.p[1])*np.sin(self.p[7])/(self.p[1]*self.p[2]*self.p[6])*u[1]
 
-        f3 = - self.u[0]*np.sin(self.p[7])/(self.p[3]*self.p[1])*x[0] \
-             - self.u[0]*self.p[6]*np.cos(self.p[7])/(self.p[3]*self.p[1])*x[1] \
-             - self.p[6]*np.sin(self.p[7])/(self.p[1]*self.p[3])*u[0]
+        f3 = - 1*np.sin(self.p[7])/(self.p[3]*self.p[1])*x[0] \
+             - 1*self.p[6]*np.cos(self.p[7])/(self.p[3]*self.p[1])*x[1] \
+             - self.p[6]*np.sin(self.p[7])/(self.p[1]*self.p[3])*u[1]
 
         return np.array([f1, f2, f3])
 
@@ -53,12 +53,12 @@ class ZIM(Model):
 
         x, u = super().g(x, u)
 
-        g1 = - self.u[0]*np.sin(self.p[7])/self.p[1]*x[0] \
-             - self.u[0]*self.p[6]*np.cos(self.p[7])/self.p[1]*x[1] \
-             + 2*self.p[4]*self.u[0] - (self.p[6]*np.sin(self.p[7])/self.p[1])*u[0]
+        g1 = - 1*np.sin(self.p[7])/self.p[1]*x[0] \
+             - 1*self.p[6]*np.cos(self.p[7])/self.p[1]*x[1] \
+             + 2*self.p[4]*1 - (self.p[6]*np.sin(self.p[7])/self.p[1])*u[1]
 
-        g2 = - self.u[0]*np.cos(self.p[7])/self.p[1]*x[0] \
-             + self.u[0]*self.p[6]*np.sin(self.p[7])/self.p[1]*x[1] \
-             + 2*self.p[5]*self.u[0] + (2*self.u[0] - self.p[6]*np.cos(self.p[7]))/self.p[1]*u[0]
+        g2 = - 1*np.cos(self.p[7])/self.p[1]*x[0] \
+             + 1*self.p[6]*np.sin(self.p[7])/self.p[1]*x[1] \
+             + 2*self.p[5]*1 + (2*self.u[0] - self.p[6]*np.cos(self.p[7]))/self.p[1]*u[1]
 
         return np.array([g1, g2])
