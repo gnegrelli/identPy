de45578ce3e517b675c54be12530591b61650bd8
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2020-02-11 20:23:52 -0300

    Upload sample data for DFIG model

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from Model.systemSM import SpringMass as mod
+from Model.systemZIM import ZIM as mod
+from Model.systemDFIG import DFIG as mod
 from Model.Implicit_Methods.RK4 import RK4
 
 from Method.MVMO import MVMO
@@ -13,26 +15,27 @@ from estimator import Estimator
 from Error.WLS_Error import _eval
 
 
-filepath = 'Sample_Data/Sample_SpringMass2.csv'
+filepath = 'Sample_Data/Sample_DFIG_Erlich_pu2.csv'
 
 with open(filepath, 'r') as f:
     for line in f.read().split('\n'):
         if line and line[0] is not '%':
-            t, x, v, u = line.split(',')
-            t = eval(t)
-            x = eval(x)
-            v = eval(v)
-            u = eval(u)
+            values = line.split(',')
+            t = eval(values[0])
+            v = eval(values[1])
+            theta = eval(values[2])
+            p = eval(values[4])
+            q = eval(values[5])
 
             try:
-                y_meas = np.vstack((y_meas, np.array([t, x, v])))
+                y_meas = np.vstack((y_meas, np.array([t, p, q])))
             except NameError:
-                y_meas = np.array([t, x, v])
+                y_meas = np.array([t, p, q])
 
             try:
-                u_meas = np.vstack((u_meas, np.array([t, u])))
+                u_meas = np.vstack((u_meas, np.array([t, v, theta, p, q])))
             except NameError:
-                u_meas = np.array([t, u])
+                u_meas = np.array([t, v, theta, p, q])
 
 # plt.figure(1)
 # plt.plot(y_meas[:, 0], y_meas[:, 1])
@@ -43,15 +46,15 @@ with open(filepath, 'r') as f:
 # plt.show()
 
 # TODO: Read input vector u_meas on the model side
-a = mod(np.array([0, 0]), np.array([0]), u_meas, RK4(final_time=2*np.pi))
-a.update_output(p=np.array([3, 6]))
+a = mod(np.array([0.995529958481552, 0.394837954500335]), u_meas[0], u_meas, RK4(final_time=1))
+a.update_output(p=np.array([0.4022/(33**2/90), 2.3861/(33**2/90), 10.516, 0.038, 0.393, 2.0, 1.1]))
 
-# plt.figure(3)
-# plt.plot(a.y[:, 0], a.y[:, 1])
-# plt.show()
+plt.figure(3)
+plt.plot(a.y[:, 0], a.y[:, 1])
+plt.show()
 
-m = MVMO(np.array([0, 0]), np.array([10, 10]), plot=True)
-# m = PSO(np.array([0, 0]), np.array([10, 10]))
+m = MVMO(np.array([0., 0., 0., 0., 0., 0., 0.8, -0.5]), np.array([.5, .2, 1., 1., 7., 5., 1.2, 0.5]))
+m = PSO(np.array([0., 0., 0., 0., 0., 0., 0.8, -0.5]), np.array([.5, .2, 1., 1., 7., 5., 1.2, 0.5]))
 # m = TS(np.array([3, 5]))
 
 est = Estimator(y_meas, a, m)
