3d46cf4d0c67c92f2f3ba1ba18447e10f6002a23
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2020-02-23 19:04:21 -0300

    Read input and output data on method

--- main.py
+++ main.py
@@ -20,32 +20,33 @@ def input_read(file_path=None, u_indices=None, y_indices=None):
     assert os.path.exists(file_path), 'Data file must exist'
     assert file_path.split('.')[-1] in ['csv', 'txt', 'dat'], 'Only .csv, .txt and .dat file supported'
 
-    pass
+    with open(file_path, 'r') as f:
+        for line in f.read().split('\n'):
+            if line and line[0] is not '%':
+                values = line.split(',')
+
+                input = np.array([eval(values[0])])
+                output = np.array([eval(values[0])])
+
+                for i in u_indices:
+                    input = np.hstack((input, np.array([eval(values[i])])))
+                for i in y_indices:
+                    output = np.hstack((output, np.array([eval(values[i])])))
+
+                try:
+                    y_m = np.vstack((y_m, output))
+                except NameError:
+                    y_m = output
+
+                try:
+                    u_m = np.vstack((u_m, input))
+                except NameError:
+                    u_m = input
+
+    return u_m, y_m
 
 
-input_read('Sample_Data/Sample_DFIG_Erlich_pu_fault.csv', u_indices=[1], y_indices=[2])
-
-filepath = 'Sample_Data/Sample_DFIG_Erlich_pu_fault.csv'
-
-with open(filepath, 'r') as f:
-    for line in f.read().split('\n'):
-        if line and line[0] is not '%':
-            values = line.split(',')
-            t = eval(values[0])
-            v = eval(values[1])
-            theta = eval(values[2])
-            p = eval(values[4])
-            q = eval(values[5])
-
-            try:
-                y_meas = np.vstack((y_meas, np.array([t, p, q])))
-            except NameError:
-                y_meas = np.array([t, p, q])
-
-            try:
-                u_meas = np.vstack((u_meas, np.array([t, v, theta, p, q])))
-            except NameError:
-                u_meas = np.array([t, v, theta, p, q])
+u_meas, y_meas = input_read('Sample_Data/Sample_DFIG_Erlich_pu_fault.csv', u_indices=[1, 2, 4, 5], y_indices=[4, 5])
 
 # plt.figure(1)
 # plt.plot(y_meas[:, 0], y_meas[:, 1])
