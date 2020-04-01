e25c5b04accf88d13758d3aa5094893545cfe2b3
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2020-04-01 18:18:27 -0300

    Initialize Estimator class in package Objects

---
+++ Objects/__init__.py
@@ -0,0 +1,1 @@
+from Objects.estimator import Estimator
--- main.py
+++ main.py
@@ -8,9 +8,7 @@ from Model import RK4
 
 from Method import MVMO, PSO, TS
 
-from estimator import Estimator
-
-from Error.WLS_Error import _eval
+from Objects import Estimator
 
 
 def input_read(file_path=None, u_indices=None, y_indices=None):
