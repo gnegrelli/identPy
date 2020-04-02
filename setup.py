104ce67d8aa23aaa83d523b03372b2fc0e899d46
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2020-04-01 21:13:18 -0300

    Create setup for identpy package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='identpy',
+    version='1.0.0',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='Gabriel Negrelli',
+    author_email='gabriel.jose.gomes@usp.br',
+    description='identPy - A tool for parameter estimation of mathematical models'
+)
