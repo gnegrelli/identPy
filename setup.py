fb5feb42f34ca447c4dbc8d5e1a2ecd2c922a8e3
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2020-04-01 21:22:02 -0300

    Add package requirements to setup

--- setup.py
+++ setup.py
@@ -4,9 +4,12 @@ setup(
     name='identpy',
     version='1.0.0',
     packages=find_packages(),
-    url='',
-    license='',
     author='Gabriel Negrelli',
     author_email='gabriel.jose.gomes@usp.br',
-    description='identPy - A tool for parameter estimation of mathematical models'
+    description='identPy - A tool for parameter estimation of mathematical models',
+    install_requires=[
+        'numpy==1.17.4',
+        'scipy==1.4.1',
+        'matplotlib==3.1.2',
+    ],
 )
