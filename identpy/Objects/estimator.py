9ef83f1544b2ffc716f5f975858f4c0e7e22ddf9
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2020-04-21 20:24:07 -0300

    Combine two estimation methods

--- identpy/Objects/estimator.py
+++ identpy/Objects/estimator.py
@@ -6,16 +6,22 @@ from identpy.Method import Method
 
 class Estimator:
 
-    def __init__(self, real_output, model, method):
-        assert isinstance(model, Model), "Model must be of class Model"
-        assert isinstance(method, Method), "Method must be of class Method"
+    def __init__(self, real_output, model, method1, method2=None):
         assert isinstance(real_output, np.ndarray), "Real output must be a numpy array"
+
+        assert isinstance(model, Model), "Model must be an instance of Model class"
+
+        assert isinstance(method1, Method), "First method must be an instance of Method class"
+        assert isinstance(method2, Method) or method2 is None, "Second method must be an instance of Method class"
 
         self.y_meas = real_output
 
         self.model = model
-        self.method = method
+        self.method1 = method1
+        self.method2 = method2
 
     def __call__(self):
 
-        self.method(self)
+        self.method1(self)
+        if self.method2:
+            self.method2(self)
--- main.py
+++ main.py
@@ -54,27 +54,33 @@ a = DFIG(np.array([0.995628884585680, 0.396820283647337]), u_meas[0], u_meas,
 a.update_output(p=np.array([0.033626225647791, 0.199200232546442, 6.977405042044428, 0.035473950408972,
                             0.269505715408350, 1.998406311405870, 1.099838352995214]))
 
-m = MVMO(np.array([0.033626225647791, 0.199200232546442, 6.977405042044428, 0.035473950408972, 0.269505715408350,
-                   1.998406311405870, 1.099838352995214])*.8,
-         np.array([0.033626225647791, 0.199200232546442, 6.977405042044428, 0.035473950408972, 0.269505715408350,
-                   1.998406311405870, 1.099838352995214])*1.1,
-         wndw_sz=3, tol=0.1)
+m1 = MVMO(np.array([0.033626225647791, 0.199200232546442, 6.977405042044428, 0.035473950408972, 0.269505715408350,
+                    1.998406311405870, 1.099838352995214])*.8,
+          np.array([0.033626225647791, 0.199200232546442, 6.977405042044428, 0.035473950408972, 0.269505715408350,
+                    1.998406311405870, 1.099838352995214])*1.1,
+          wndw_sz=3, tol=0.1)
 
-est = Estimator(y_meas, a, m)
+m2 = TS(np.array([1, 1, 1, 1, 1, 1, 1]))
+
+est = Estimator(y_meas, a, m1, m2)
 
 est()
 
 plt.figure()
 plt.plot(est.model.y[:, 0], est.model.y[:, 1], label='model')
-plt.plot(y_meas[:, 0], y_meas[:, 1], label='real')
+plt.plot(y_meas[:, 0], y_meas[:, 1], '--', label='real')
 plt.legend()
 
 plt.figure()
 plt.plot(est.model.y[:, 0], est.model.y[:, 2], label='model')
-plt.plot(y_meas[:, 0], y_meas[:, 2], label='real')
+plt.plot(y_meas[:, 0], y_meas[:, 2], '--', label='real')
 plt.legend()
 
 plt.figure()
-plt.plot(est.method.error_log)
+plt.plot(est.method1.error_log)
+
+if est.method2:
+    plt.figure()
+    plt.plot(est.method2.error_log)
 
 plt.show()
