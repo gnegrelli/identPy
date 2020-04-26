fdae833aefa9c6dbb389b540e223b5604ae658b5
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2020-04-26 12:13:30 -0300

    Create attributes plot and verbose for method base class

--- identpy/Method/method.py
+++ identpy/Method/method.py
@@ -3,9 +3,12 @@ from abc import ABC
 
 class Method(ABC):
 
-    def __init__(self):
+    def __init__(self, plot=False, verbose=False):
         self.counter = 0
         self.error_log = []
+
+        self.plot = plot
+        self.verbose = verbose
 
     def __call__(self, *args, **kwargs):
         pass
