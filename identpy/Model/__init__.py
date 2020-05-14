174c650405c757c1b7498e10c675f0b2e660c7a0
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2020-05-13 21:41:24 -0300

    Initialize new model

--- identpy/Model/__init__.py
+++ identpy/Model/__init__.py
@@ -6,3 +6,4 @@ from identpy.Model.systemSM import SpringMass
 from identpy.Model.systemPend import Pendulum
 from identpy.Model.systemZIM import ZIM
 from identpy.Model.systemDFIG import DFIG
+from identpy.Model.improved_DFIG import improved_DFIG
