0205346567216f8126dc5dceb71acb3dbd9b4196
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2020-06-13 20:10:09 -0300

    Convert data from powerfactory to raw csv

--- scripts/adapt_data_powerfactory.py
+++ scripts/adapt_data_powerfactory.py
@@ -1,9 +1,26 @@
+import re
 import argparse
+
 import pandas as pd
 
 
 def adapt_data_pf(path):
-    pass
+    df = pd.read_csv(path, header=None)
+    df = df[df[0] == 'other']
+    df = df[df[1].str.startswith(' ')]
+
+    df.reset_index(drop=True, inplace=True)
+
+    data = df[1].to_list()
+    data = [re.sub(r'^\s+', '', d) for d in data]
+    data = [re.sub(r'\s+', ',', d) for d in data]
+
+    with open('test.csv', 'w+') as f:
+        print(data[0], file=f)
+        for row in data[1:]:
+            if row.startswith('Time'):
+                continue
+            print(row, file=f)
 
 
 if __name__ == '__main__':
