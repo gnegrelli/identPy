1220ca18f348b8b8ff16cc5a08ebfc89992570a2
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2020-06-14 10:50:48 -0300

    Add output path in argparse

--- scripts/adapt_data_powerfactory.py
+++ scripts/adapt_data_powerfactory.py
@@ -1,10 +1,11 @@
 import re
+import pathlib
 import argparse
 
 import pandas as pd
 
 
-def adapt_data_pf(path):
+def adapt_data_pf(path, output_path=None):
     df = pd.read_csv(path, header=None)
     df = df[df[0] == 'other']
     df = df[df[1].str.startswith(' ')]
@@ -15,8 +16,13 @@ def adapt_data_pf(path):
     data = [re.sub(r'^\s+', '', d) for d in data]
     data = [re.sub(r'\s+', ',', d) for d in data]
 
-    with open('test.csv', 'w+') as f:
-        print(data[0], file=f)
+    if output_path:
+        output_file = output_path / 'data_from_powerfactory.csv'
+    else:
+        output_file = 'data_from_powerfactory.csv'
+
+    with open(output_file, 'w+') as f:
+        print('% ' + data[0], file=f)
         for row in data[1:]:
             if row.startswith('Time'):
                 continue
@@ -26,6 +32,15 @@ def adapt_data_pf(path):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Adapt data exported from DIgSILENT PowerFactory to identpy standard')
     parser.add_argument('filepath', help='Path of datafile exported from PowerFactory', type=argparse.FileType('r'))
+    parser.add_argument('-o', '--output', help='Output path')
 
     args = parser.parse_args()
-    adapt_data_pf(args.filepath)
+
+    if args.output:
+        output = pathlib.Path(args.output)
+        assert output.is_dir(), 'Output path must be a valid directory'
+    else:
+        dir_ = pathlib.Path(args.filepath.name)
+        output = dir_.parent
+
+    adapt_data_pf(args.filepath, output)
