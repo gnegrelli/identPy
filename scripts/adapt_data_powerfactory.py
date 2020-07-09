9447f174a6be4d86cbf5a8446e82c68972582fa3
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2020-07-09 14:09:49 -0300

    Assert arguments and move path creation to method

--- scripts/adapt_data_powerfactory.py
+++ scripts/adapt_data_powerfactory.py
@@ -6,9 +6,23 @@ import pandas as pd
 
 
 def adapt_data_pf(path, output_path=None, time_step=None):
+    assert pathlib.Path(path).is_file(), 'Filepath \'{}\' does not exist'.format(path)
+    assert path.endswith('.csv'), 'Only .csv files are supported'
+
+    assert isinstance(output_path, str) or output_path is None, 'Path of output must be a string'
+
+    assert isinstance(time_step, float) and time_step > 0, 'Value of time step must be a positive real number'
+
+    # Get path where file will be saved
+    if output_path:
+        output = pathlib.Path(output_path)
+        assert output.is_dir(), 'Output path must be a valid directory'
+    else:
+        output = pathlib.Path(path).parent
+    output = output / 'data_from_powerfactory.csv'
+
     df = pd.read_csv(path, header=None)
-    df = df[df[0] == 'other']
-    df = df[df[1].str.startswith(' ')]
+    df = df[(df[0] == 'other') & (df[1].str.startswith(' '))]
 
     df.reset_index(drop=True, inplace=True)
 
@@ -16,12 +30,7 @@ def adapt_data_pf(path, output_path=None, time_step=None):
     data = [re.sub(r'^\s+', '', d) for d in data]
     data = [re.sub(r'\s+', ',', d) for d in data]
 
-    if output_path:
-        output_file = output_path / 'data_from_powerfactory.csv'
-    else:
-        output_file = 'data_from_powerfactory.csv'
-
-    with open(output_file, 'w+') as f:
+    with open(output, 'w+') as f:
         print('% ' + data[0], file=f)
         for row in data[1:]:
             if row.startswith('Time'):
@@ -31,17 +40,10 @@ def adapt_data_pf(path, output_path=None, time_step=None):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Adapt data exported from DIgSILENT PowerFactory to identpy standard')
-    parser.add_argument('filepath', help='Path of datafile exported from PowerFactory', type=argparse.FileType('r'))
-    parser.add_argument('-o', '--output', help='Output path')
+    parser.add_argument('filepath', help='Path of datafile exported from PowerFactory', type=str)
+    parser.add_argument('-o', '--output', help='Output path', type=str, default=None)
     parser.add_argument('-t', '--time_step', help='Time step between data points', type=float, default=None)
 
     args = parser.parse_args()
 
-    if args.output:
-        output = pathlib.Path(args.output)
-        assert output.is_dir(), 'Output path must be a valid directory'
-    else:
-        dir_ = pathlib.Path(args.filepath.name)
-        output = dir_.parent
-
-    adapt_data_pf(args.filepath, output, args.time_step)
+    adapt_data_pf(args.filepath, args.output, args.time_step)
