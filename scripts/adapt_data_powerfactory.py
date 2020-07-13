73468e9b9fe25feb9e40ad0d103fff5060ecc346
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2020-07-13 18:43:08 -0300

    Interpolate values in case of adjustments needed

--- scripts/adapt_data_powerfactory.py
+++ scripts/adapt_data_powerfactory.py
@@ -5,13 +5,20 @@ import argparse
 import pandas as pd
 
 
-def adapt_data_pf(path, output_path=None, time_step=None, initial_time=0, final_time=1):
+def adapt_data_pf(path, output_path=None, time_step=None, initial_time=None, final_time=None):
     assert pathlib.Path(path).is_file(), 'Filepath \'{}\' does not exist'.format(path)
     assert path.endswith('.csv'), 'Only .csv files are supported'
 
     assert isinstance(output_path, str) or output_path is None, 'Path of output must be a string'
 
-    assert isinstance(time_step, float) and time_step > 0, 'Value of time step must be a positive real number'
+    if time_step is not None:
+        assert isinstance(time_step, (float, int)) and time_step > 0, 'Time step must be a positive real number'
+
+    if initial_time is not None:
+        assert isinstance(initial_time, (float, int)), 'Initial time must be a real number'
+
+    if final_time is not None:
+        assert isinstance(final_time, (float, int)), 'Final time must be a real number'
 
     # Get path where file will be saved
     if output_path:
@@ -21,17 +28,44 @@ def adapt_data_pf(path, output_path=None, time_step=None, initial_time=0, final_
         output = pathlib.Path(path).parent
     output = output / 'data_from_powerfactory.csv'
 
+    # Get relevant data from file
     pat = re.compile(r'(^\"other\","\s+)(.*)(\"$)')
     with open(path, 'r') as file:
         data = [re.search(pat, row).group(2).split() for row in file.read().split('\n') if re.search(pat, row)]
 
-    cols = data[0]
-    data = [d for d in data if 'Time' not in d]
+    # Create dataframe from data
+    df = pd.DataFrame(data=[d for d in data if 'Time' not in d], columns=data[0], dtype=float)
+    df.set_index('Time', inplace=True)
 
-    df1 = pd.DataFrame(data, columns=cols, dtype=float)
-    df1.set_index('Time', inplace=True)
+    if time_step is None:
+        # If no adjustments in time step are needed, save dataframe in output
+        df.to_csv(output, float_format='%.6f')
+    else:
+        # Create dataframe for time step adjustments
+        df_adj = pd.DataFrame(columns=df.columns)
+        df_adj.index.name = df.index.name
 
-    df1.to_csv(output)
+        # Get value of initial instant
+        t = df.index[0]
+        if initial_time is not None:
+            t = max(t, initial_time)
+
+        # Get value of final instant
+        tf = df.index[-1]
+        if final_time is not None:
+            tf = min(tf, final_time)
+
+        while len(df.loc[df.index > t]) and t <= tf:
+            # Get values on dataframe for instants immediately before and after time 't'
+            prior = df.loc[df.index <= t].iloc[-1]
+            post = df.loc[df.index > t].iloc[0]
+
+            # Interpolate values and save result on adjusted dataframe
+            df_adj.loc[t] = prior + (post - prior) * (t - prior.name) / (post.name - prior.name)
+            t = round(t + time_step, 6)
+
+        # Save adjusted dataframe in output
+        df_adj.to_csv(output, float_format='%.6f')
 
 
 if __name__ == '__main__':
